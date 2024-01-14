93ace70ef42a812a33c8bdfbb4f6bc766e5f2407
KuJoe <me@kujoe.net>
2024-01-14 00:58:58 -0500

    Fixed streaming.php
    The cURL check in streaming.php was returning "OK" even when the service was offline. Fixed to use a simpler method of getting the status.

--- streaming.php
+++ streaming.php
@@ -1,37 +1,12 @@
 <?php
 
-function get_web_page($url) {
-    $options = array(
-        CURLOPT_RETURNTRANSFER => true,   // return web page
-        CURLOPT_HEADER         => false,  // don't return headers
-        CURLOPT_FOLLOWLOCATION => true,   // follow redirects
-        CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
-        CURLOPT_ENCODING       => "",     // handle compressed
-        CURLOPT_USERAGENT      => "monitor", // name of client
-        CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
-        CURLOPT_CONNECTTIMEOUT => 30,    // time-out on connect
-        CURLOPT_TIMEOUT        => 30,    // time-out on response
-    );
+$url = 'http://<hostname/IP>:4000/api/v1/streaming/health';
+$content = file_get_contents($url);
 
-    $ch = curl_init($url);
-    curl_setopt_array($ch, $options);
-
-    $content  = curl_exec($ch);
-
-    curl_close($ch);
-
-    return $content;
-}
-
-$check = '0';
-$response = get_web_page("http://<hostname/IP>:4000/api/v1/streaming/health");
-if($response = "OK") {
-        $check++;
-}
-if($check > 0) {
-        echo "HEALTHY";
+if ($content == "OK" ) {
+    echo "HEALTHY";
 } else {
-        echo "DEGRADED";
+    echo "DEGRADED";
 }
 
 ?>
